26cfc571c48293af349c0c6460355dfc22d65d66
stealthviper786 <stealthviper786@gmail.com>
2025-03-26 10:57:51 +0500

    update 8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Prisma Volt Studios",
+  title: "Stealth Viper",
   description: "Bringing Dreams to Life Through Interactive Games",
 };
 
